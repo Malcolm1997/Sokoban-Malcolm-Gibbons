977793ed26de44cafb050f5613e6f64fb8f9c329
Malcolm Alfredo Gibbons <malcolm.gibbons1997@gmail.com>
2021-06-23 20:12:45 -0300

    Inicio del proyecto

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Direction{
+    up,down,right,left
+}
+
+
+
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Direction direction;
+    public float movPJ = 2f;
+    Vector3 myPosition;
+    public LayerMask cajaMov;
+    public LayerMask obstaculo;
+
+    void Start()
+    {
+        myPosition = transform.position;
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = Vector2.zero;
+        RaycastHit2D rh;
+        RaycastHit2D rhMov;
+        RaycastHit2D rhMovObs;
+        
+        //Direccion de deteccion
+
+        if (myPosition == transform.position)
+        {
+            if (Input.GetKeyDown(KeyCode.A)) dir = Vector2.left;
+            if (Input.GetKeyDown(KeyCode.D)) dir = Vector2.right;
+            if (Input.GetKeyDown(KeyCode.W)) dir = Vector2.up;
+            if (Input.GetKeyDown(KeyCode.S)) dir = Vector2.down;
+
+        }
+
+
+        rh = Physics2D.Raycast(transform.position, dir, 1, obstaculo);
+        rhMov = Physics2D.Raycast(transform.position, dir, 1, cajaMov);
+        rhMovObs = Physics2D.Raycast(transform.position, dir, 2, obstaculo);
+
+        // Ejecucion del moviminto
+
+        if (myPosition == transform.position)
+        {   
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (!rh.collider)
+                {
+                    if(!rhMov.collider && !rhMovObs.collider || rhMov.collider && !rhMovObs.collider || !rhMov.collider && rhMovObs.collider)
+                    {
+                        myPosition += Vector3.left;
+                    }
+                }               
+                direction = Direction.left;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (!rh.collider)
+                {
+                    if (!rhMov.collider && !rhMovObs.collider || rhMov.collider && !rhMovObs.collider || !rhMov.collider && rhMovObs.collider)
+                    {
+                        myPosition += Vector3.right;
+                    }
+                }
+                direction = Direction.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                if (!rh.collider)
+                {
+                    if (!rhMov.collider && !rhMovObs.collider || rhMov.collider && !rhMovObs.collider || !rhMov.collider && rhMovObs.collider)
+                    {
+                        myPosition += Vector3.up;
+                    }
+                }
+                direction = Direction.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (!rh.collider)
+                {
+                    if (!rhMov.collider && !rhMovObs.collider || rhMov.collider && !rhMovObs.collider || !rhMov.collider && rhMovObs.collider)
+                    {
+                        myPosition += Vector3.down;
+                    }
+                }
+                direction = Direction.down;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, myPosition, movPJ * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Obstaculo"))
+        {
+            Debug.Log("hola");
+        }
+    }
+
+}
---
+++ Assets/Scripts/MoveObj.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MoveObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector2 dir;
+    Vector2 position;
+    Vector2 dirObj;
+    public LayerMask pj;
+    public LayerMask Obs;
+
+    float movPJ = 2f;
+    void Start()
+    {
+        position = transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // deteccion de el empuje
+        dir = Vector2.zero;
+            if (Input.GetKeyDown(KeyCode.A)) dir = Vector2.right;
+            if (Input.GetKeyDown(KeyCode.D)) dir = Vector2.left;
+            if (Input.GetKeyDown(KeyCode.W)) dir = Vector2.down;
+            if (Input.GetKeyDown(KeyCode.S)) dir = Vector2.up;
+            
+
+        RaycastHit2D rh;
+        RaycastHit2D rhObs;
+        rh = Physics2D.Raycast(position, dir, 1f, pj);
+        rhObs = Physics2D.Raycast(position, -dir, 1f, Obs);
+
+        // Movimiento del objeto
+        if(rhObs.collider == null)
+        {
+            if (rh.collider != null)
+            {
+                if (transform.position.x == position.x && transform.position.y == position.y)
+                {
+                    if (Input.GetKeyDown(KeyCode.D))
+                    {
+                        position -= dir;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        position -= dir;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.S))
+                    {
+                        position -= dir;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        position -= dir;
+                    }
+                }
+            }
+        }
+            
+        transform.position = Vector2.MoveTowards(transform.position, position, movPJ * Time.deltaTime);
+    }
+}
